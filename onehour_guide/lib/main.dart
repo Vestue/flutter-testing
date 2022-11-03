182f5ee119d19a835a8be0a09241acadc7d74144
Vestue <61783376+Vestue@users.noreply.github.com>
2022-11-03 22:42:58 +0100

    Add home_page.dart

--- onehour_guide/lib/main.dart
+++ onehour_guide/lib/main.dart
@@ -27,6 +27,7 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,9 +36,22 @@ class _RootPageState extends State<RootPage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          debugPrint("Floating Action Button");
+          debugPrint("Mush added.");
         },
         child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        onDestinationSelected: (int index){
+          setState(() {
+            currentPage = index;
+          });
+        currentPage = index;
+        },
+        selectedIndex: currentPage,
       ),
     );
   }
