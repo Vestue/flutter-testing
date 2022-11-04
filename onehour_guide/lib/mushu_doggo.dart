7ddcde2694be6a05cd0f3736e4470e2efbac6c25
Vestue <61783376+Vestue@users.noreply.github.com>
2022-11-04 09:22:29 +0100

    Make view scrollable

--- onehour_guide/lib/mushu_doggo.dart
+++ onehour_guide/lib/mushu_doggo.dart
@@ -23,88 +23,93 @@ class _MushuBestPageState extends State<MushuBestPage> {
           icon: const Icon(Icons.arrow_back),
         ),
       ),
-      body: Column(
-        children: [
-          Image.asset('images/cutemush.jpg'),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.black,
-          ),
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            padding: const EdgeInsets.all(10.0),
-            color: Colors.green,
-            width: double.infinity,
-            child: const Center(
-              child: Text(
-                'This is a mush.',
-                style: TextStyle(
-                  color: Colors.white,
-                  //fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('images/cutemush.jpg'),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.green,
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  'This is a mush.',
+                  style: TextStyle(
+                    color: Colors.white,
+                    //fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: isSwitch ? Colors.blue : Colors.pink,
-            ),
-            onPressed: () {
-              debugPrint('Mush button pressed');
-            },
-            child: const Text('Mush button'),
-          ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint('Outlined button pressed');
-            },
-            child: const Text('Outlined mush'),
-          ),
-          TextButton(
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: isSwitch ? Colors.blue : Colors.pink,
+              ),
               onPressed: () {
-                debugPrint('Text button');
+                debugPrint('Mush button pressed');
               },
-              child: const Text(
-                'The Mushu is a rare being, only a few have been blessed by the Mush.',
-                style: TextStyle(color: Colors.white),
-              )),
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              debugPrint('Testing gestures');
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Icon(
-                  Icons.traffic,
-                  color:  Colors.blue,
-                ),
-                Text('Blue mush?'),
-                Icon(
-                  Icons.museum,
-                  color: Colors.blue,
-                ),
-              ],
+              child: const Text('Mush button'),
             ),
-          ),
-          Switch(
-              value: isSwitch,
-              onChanged: (bool newBool) {
-                setState(() {
-                  isSwitch = newBool;
-                });
-              }),
-          Checkbox(
-              value: isCheckbox,
-              onChanged: (bool? newBool){
-                setState(() {
-                  isCheckbox = newBool;
-                });
-              }),
-        ],
+            OutlinedButton(
+              onPressed: () {
+                debugPrint('Outlined button pressed');
+              },
+              child: const Text('Outlined mush'),
+            ),
+            TextButton(
+                onPressed: () {
+                  debugPrint('Text button');
+                },
+                child: const Text(
+                  'The Mushu is a rare being, only a few have been blessed by the Mush.',
+                  style: TextStyle(color: Colors.white),
+                )),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint('Testing gestures');
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(
+                    Icons.traffic,
+                    color: Colors.blue,
+                  ),
+                  Text('Blue mush?'),
+                  Icon(
+                    Icons.museum,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+                value: isSwitch,
+                onChanged: (bool newBool) {
+                  setState(() {
+                    isSwitch = newBool;
+                  });
+                }),
+            Checkbox(
+                value: isCheckbox,
+                onChanged: (bool? newBool) {
+                  setState(() {
+                    isCheckbox = newBool;
+                  });
+                }),
+            Image.network(
+                'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.trustedreviews.com%2Fwp-content%2Fuploads%2Fsites%2F54%2F2021%2F02%2FRickrolling-in-4K-1024x662.jpg&f=1&nofb=1&ipt=c77415402d52b12c7190a17a0afd1d810e37cdc279e01ba1f2ace596088bb77b&ipo=images'
+            )
+          ],
+        ),
       ),
     );
   }
