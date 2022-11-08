de80e3a5a33977ecc3f93828ccb49e86f84fe893
Vestue <61783376+Vestue@users.noreply.github.com>
2022-11-08 16:50:19 +0100

    Update building layouts

--- building_layouts/lib/main.dart
+++ building_layouts/lib/main.dart
@@ -13,14 +13,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  /*2*/
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: const Text(
+                      'Some lake somewhere',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(
+                    'Some country',
+                    style: TextStyle(color: Colors.grey[500]),
+                  ),
+                ],
+              ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.yellow[500],
+          ),
+          const Text('42')
+        ],
+      ),
+    );
+
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Column(
+          children: [
+            titleSection,
+          ],
         ),
       ),
     );
