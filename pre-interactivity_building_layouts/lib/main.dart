99ecd2113709d0a24672ff31d90b9ae6526d8073
Vestue <61783376+Vestue@users.noreply.github.com>
2022-11-11 22:23:31 +0100

    Backup pre-interactivity layouts

---
+++ pre-interactivity_building_layouts/lib/main.dart
@@ -0,0 +1,123 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  /*2*/
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: const Text(
+                      'Some mush somewhere',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(
+                    'In some country',
+                    style: TextStyle(color: Colors.grey[500]),
+                  ),
+                ],
+              ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.yellow[500],
+          ),
+          const Text('42')
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Row (
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, "CALL"),
+        _buildButtonColumn(color, Icons.near_me, "ROUTE"),
+        _buildButtonColumn(color, Icons.share, "SHARE"),
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Jean-François Champollion cosmic fugue astonishment take root and'
+        'flourish Sea of Tranquility circumnavigated. Gathered by gravity stirred'
+        'by starlight corpus callosum concept of the number one are creatures of'
+        'the cosmos citizens of distant epochs. Dispassionate extraterrestrial'
+        'observer something incredible is waiting to be known a very small stage'
+        'in a vast cosmic arena vanquish the impossible the ash of stellar alchemy'
+        'paroxysm of global death. Extraplanetary with pretty stories for which'
+        'theres little good evidence with pretty stories for which theres little'
+        'good evidence hundreds of thousands with pretty stories for which theres'
+        'little good evidence with pretty stories for which theres little good'
+        'evidence and billions upon billions upon billions upon billions upon'
+        'billions upon billions upon billions.',
+        softWrap: true,
+      ),
+    );
+
+    Widget imageSection = Image.asset(
+      'images/top_pom.png',
+      width: 600,
+      height: 240,
+      fit: BoxFit.cover,
+    );
+
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Layout test'),
+        ),
+        body: ListView(
+          children: [
+            imageSection,
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color,),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
